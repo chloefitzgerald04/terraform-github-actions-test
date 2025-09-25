8728e3fa62734d6a1a706898a5f2db182fcd845d
C Fitzgerald <84927940+chloefitzgerald04@users.noreply.github.com>
2025-09-25 21:02:25 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+     terraform {
+       backend "s3" {}
+     }
+
+     # An example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
