872f7cb574ffdeaf2dd4affc74b3bab26c4fa76e
C Fitzgerald <84927940+chloefitzgerald04@users.noreply.github.com>
2025-09-25 21:18:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 terraform {
-  backend "s3" {}
+    backend "s3" {
+    endpoints = {
+      s3 = "https://s3.eu-central-003.backblazeb2.com"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    region                      = "eu-central-003"
+    bucket                      = $bucket
+    key                         = "actions-test/terraform.tfstate"
+  }
 }
 
 # An example resource that does nothing.
