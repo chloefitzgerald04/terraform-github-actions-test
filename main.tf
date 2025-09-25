f61ee1ffeeaf59e6c2cdd5fc6e01c9d9fdbcba48
C Fitzgerald <84927940+chloefitzgerald04@users.noreply.github.com>
2025-09-25 21:10:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-     terraform {
-       backend "s3" {}
-     }
+terraform {
+  backend "s3" {}
+}
 
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
