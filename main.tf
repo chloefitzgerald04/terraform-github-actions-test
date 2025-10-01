a64c8f5384f384e161875c3986dc2f57cbb6dfa7
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2025-10-01 21:29:35 +0000

    test

--- main.tf
+++ main.tf
@@ -20,55 +20,8 @@ terraform {
   }
 }
 
-provider "proxmox" {
-  pm_api_url = var.api_url
-  pm_api_token_id = var.token_id
-  pm_api_token_secret = var.token_secret
-  pm_tls_insecure = true
-}
 
-resource "proxmox_vm_qemu" "arch" {
-    count                     = 0
-    name                      = "cf-pve-cl-01-docker-${count.index + 1}"
-    #pxe                       = true
-    agent                     = 0
-    automatic_reboot          = true
-    bios                      = "seabios"
-    boot                      = "order=scsi0;ide2;net0"
-    hotplug                   = "network,disk,usb"
-    kvm                       = true
-    memory                    = var.mem
-    onboot                    = false
-    scsihw                    = "virtio-scsi-pci"
-    target_node               = var.proxmox_host
-    cores       = 2
-    sockets     = 1
+module "proxmox_vm" {
+  source           = "./modules/vms"
 
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    discard            = true
-                    emulatessd         = true
-                    iothread           = true
-                    size               = 32
-                    storage            = "Ceph"
-                }
-            }
-        }
-        ide {
-            ide2 {
-                cdrom {
-                    iso = var.iso
-                 }
-            }
-        }
-    }
-
-    network {
-        bridge    = var.nic_name
-        firewall  = false
-        link_down = false
-        model     = "virtio"
-    }
 }
