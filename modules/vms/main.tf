8a50b476d473cc17fb39f425ad0470f8d90a5389
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2025-10-04 20:10:52 +0000

    cleanup + adding modules

--- modules/vms/main.tf
+++ modules/vms/main.tf
@@ -6,22 +6,24 @@ terraform {
     }
   }
 }
-resource "proxmox_vm_qemu" "arch" {
-    count                     = 0
-    name                      = "cf-pve-cl-01-docker-${count.index + 1}"
-    #pxe                       = true
+resource "proxmox_vm_qemu" "iso_vms" {
+    for_each                  = var.iso_vms
+    name                      = each.value.name
+    pxe                       = try(each.value.pxe, false)
     agent                     = 1
     automatic_reboot          = true
-    bios                      = "seabios"
+    bios                      = try(each.value.bios, "seabios")
     boot                      = "order=scsi0;ide2;net0"
     hotplug                   = "network,disk,usb"
     kvm                       = true
-    memory                    = var.mem
-    onboot                    = false
+    memory                    = try(each.value.mem, "4096")
+    onboot                    = try(each.value.startup, true)
     scsihw                    = "virtio-scsi-pci"
-    target_node               = var.proxmox_host
-    cores       = 2
-    sockets     = 1
+    target_node               = each.value.node
+    cores                     = 2
+    sockets                   = 1
+    full_clone = true
+    clone_wait = 0
 
     disks {
         scsi {
@@ -29,25 +31,62 @@ resource "proxmox_vm_qemu" "arch" {
                 disk {
                     discard            = true
                     emulatessd         = true
-                    iothread           = true
-                    size               = 32
+                    size               = try(each.value.disk_size, "32")
                     storage            = "Ceph"
                 }
             }
-        }
+        }    
         ide {
             ide2 {
                 cdrom {
-                    iso = var.iso
-                 }
+                    iso = try(each.value.iso, null)
+                }
             }
         }
     }
 
     network {
-        bridge    = var.nic_name
+        bridge    = try(each.value.network_name, "vmbr2")
         firewall  = false
         link_down = false
-        model     = "virtio"
+        model     = try(each.value.nic_model, "virtio")
+    }
+}
+
+resource "proxmox_vm_qemu" "clone_vms" {
+    for_each                  = var.clone_vms
+    name                      = each.value.name
+    agent                     = 1
+    automatic_reboot          = true
+    bios                      = try(each.value.bios, "seabios")
+    hotplug                   = "network,disk,usb"
+    kvm                       = true
+    memory                    = try(each.value.mem, "4096")
+    onboot                    = try(each.value.startup, true)
+    scsihw                    = "virtio-scsi-pci"
+    target_node               = each.value.node
+    cores                     = 2
+    sockets                   = 1
+    full_clone                = true
+    clone_wait                = 0
+    clone                     = try(each.value.clone, null)
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    discard            = true
+                    emulatessd         = true
+                    size               = try(each.value.disk_size, "32")
+                    storage            = "Ceph"
+                }
+            }
+        }
+    }
+
+    network {
+        bridge    = try(each.value.network_name, "vmbr2")
+        model     = try(each.value.nic_model, "virtio")
+        tag       = try(each.value.vlan, 10)
     }
 }
--- modules/vms/vars.tf
+++ modules/vms/vars.tf
@@ -1,28 +1,62 @@
-variable "ssh_key" {
-  default = "your_public_ssh_key_here"
-}
-variable "proxmox_host" {
-    default = "pve-02"
-}
-variable "iso" {
-    default = "NAS:iso/archlinux-2024.06.01-x86_64.iso"
-}
-variable "nic_name" {
-    default = "vmbr2"
-}
-variable "vlan_num" {
-    default = "10"
-}
-variable "mem" {
-    default = "4096"
+variable "default_vm"{
+    default = {}
 }
 
-variable "api_url" {
-    default = "https://10.0.0.102:8006/api2/json"
+variable "iso_vms" {
+#     default = {
+#         "VM1" = {
+#             "name" = "01"
+#             "node" = "pve-01"
+#             "procs" = 1
+#             "mem" = 4096
+#             "network_name" = "vmbr2"
+#             "iso" = "NAS:iso/archlinux-2024.06.01-x86_64.iso"
+#             "pxe" = true
+#             "startup" = false
+#             "bios" = "ovmf"
+#             "disk_size" = 16
+#         }
+#         "VM2" = {
+#             "name" = "02"
+#             "node" = "pve-02"
+#             "procs" = 1
+#             "mem" = 4096
+#             "network_name" = "vmbr2"
+#             "iso" = "NAS:iso/archlinux-2024.06.01-x86_64.iso"
+#             "disk_size" = 32
+            
+            
+#         }
+#         "VM3" = {
+#             "name" = "03"
+#             "node" = "pve-03"
+#             "procs" = 1
+#             "mem" = 4096
+#             "network_name" = "vmbr2"
+#             "iso" = "NAS:iso/archlinux-2024.06.01-x86_64.iso"
+#             "nic_model" = "vmxnet3"
+#             "disk_size" = 16
+#         }
+#   }
+    default = {}
 }
-variable "token_secret" {
-    default = "123abc"
-}
-variable "token_id" {
-    default = "test@pve!test"
+
+
+
+variable "clone_vms" {
+    # default = {
+    #     "VM4" = {
+    #         "name" = "04"
+    #         "node" = "pve-02"
+    #         "procs" = 2
+    #         "mem" = 4096
+    #         "network_name" = "vmbr2"
+    #         "vlan" = 20
+    #         "clone" = "flatcar-qemu"
+    #         "startup" = false
+    #         "bios" = "seabios"
+    #         "disk_size" = 16
+    #     }
+    # }
+    default = {}
 }
